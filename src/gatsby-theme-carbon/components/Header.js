b4ba243f3a4003361994b8094aa233327b905ada
Connor Leech <connor@ibm.com>
2019-06-07 16:28:49 -0500

    docs: game time

---
+++ src/gatsby-theme-carbon/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'gatsby-theme-carbon/src/components/Header';
+
+const CustomSwitcher = props => (
+  <Header {...props}>
+    <span>Carbon</span>&nbsp;Design System
+  </Header>
+);
+
+export default CustomSwitcher;
